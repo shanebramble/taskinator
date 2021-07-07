3cb3884d96a63be503e6939551b53dbfe8d556a5
Shane Wade <sbramwade@msn.com>
2021-07-07 14:24:04 -0400

    added forms with submit feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function ()
+var createTaskHandler = function (event)
 {
+    event.preventDefault();
+
     var listItem1 = document.createElement("li");
     listItem1.className = "task-item";
     listItem1.textContent = "This is a new task.";
     tasksToDoE1.appendChild(listItem1);
-}
+};
 
-buttonEl.addEventListener("click",createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
